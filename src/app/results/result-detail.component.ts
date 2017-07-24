2df143bd212246a98bad82243e49b32a33682811
Sergey Ladutko <verus774@mail.ru>
2017-07-24 17:03:35 +0300

    pretty view for results in ResultDetailComponent added

--- src/app/results/result-detail.component.ts
+++ src/app/results/result-detail.component.ts
@@ -2,20 +2,36 @@ import {Component, OnInit} from '@angular/core';
 import {ActivatedRoute} from '@angular/router';
 import {Location} from '@angular/common';
 import {IResult} from './result';
+import {GroupByPipe} from 'ngx-pipes/src/app/pipes/array/group-by';
+import {PairsPipe} from 'ngx-pipes/src/app/pipes/object/pairs';
 
 
 @Component({
-  templateUrl: 'result-detail.component.html'
+  templateUrl: 'result-detail.component.html',
+  providers: [
+    GroupByPipe,
+    PairsPipe
+  ]
 })
 
 export class ResultDetailComponent implements OnInit {
   result: IResult;
+  filteredResults: any [];
 
-  constructor(private _route: ActivatedRoute, private _location: Location) {
+  constructor(private _route: ActivatedRoute,
+              private _location: Location,
+              private _pairsPipe: PairsPipe,
+              private _groupByPipe: GroupByPipe) {
   }
 
   ngOnInit(): void {
     this.result = this._route.snapshot.data['result'];
+
+    /*
+        const groupedResult = this._groupByPipe.transform(this.result.results, 'questionText');
+        this.filteredResults = this._pairsPipe.transform(groupedResult);
+    */
+
   }
 
   onBack(): void {
